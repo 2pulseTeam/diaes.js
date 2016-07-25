c946de9f135a1fd8bf9f4ea82e6800b2b21e57c7
Maxence Pinson <maxence.pinson@gmail.com>
2016-07-25 16:20:26 +0200

    Suppression du click, passage au Vorbis si possible et debug

--- js/lib/diaes.js
+++ js/lib/diaes.js
@@ -32,16 +32,43 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	 *   	Reader.getFragmentPath(number);
 	 *
 	 * - Then, it encodes every fragment using AES-CBC, with
-	 *   a different 128 bit key for each fragment. This
+	 *   a different 256 bit key for each fragment. This
 	 *   makes the audio files unreadable unless decoded.
 	 */
 
-	const FRAGMENT_DURATION = 15.0;
+	var FRAGMENT_DURATION = 15.0;
 
-	const STATE_PAUSED    = 0;
-	const STATE_PLAYING   = 1;
-	const STATE_BUFFERING = 2;
-	const STATE_ENDED     = 3;
+	var STATE_PAUSED    = 0;
+	var STATE_PLAYING   = 1;
+	var STATE_BUFFERING = 2;
+	var STATE_FINISHED  = 3;
+
+	/**
+	 * Appends two AudioBuffers into a new one.
+	 * 
+	 * @param {AudioBuffer} buffer1 The first buffer.
+	 * @param {AudioBuffer} buffer2 The second buffer.
+	 */
+	function appendBuffer(context, buffer1, buffer2) {
+		var numberOfChannels = Math.min( buffer1.numberOfChannels, buffer2.numberOfChannels );
+		var tmp = context.createBuffer( numberOfChannels, (buffer1.length + buffer2.length), buffer1.sampleRate );
+		for (var i=0; i<numberOfChannels; i++) {
+			var channel = tmp.getChannelData(i);
+			channel.set( buffer1.getChannelData(i), 0);
+			channel.set( buffer2.getChannelData(i), buffer1.length);
+		}
+		return tmp;
+	}
+
+	/**
+	 * Returns whether the browser is capable of playing
+	 * Vorbis-encoded .ogg files.
+	 */
+	function canPlayVorbis() {
+		var a = document.createElement('audio');
+
+		return !!(a.canPlayType && a.canPlayType('audio/ogg; codecs="vorbis"').replace(/no/, ''));
+	}
 
 	/**
 	 * An audio source. Communicates with the Web Audio API to
@@ -49,17 +76,17 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	 * information for that buffer.
 	 */
 	var Source = function (buffer, queue) {
-		console.log('Instanciated source', buffer, queue);
+		// console.log('Instanciated source', buffer, queue);
 
 		this.buffer = buffer;              // Audio buffer
 		this.queue  = queue;               // Parent queue
 		this.reader = queue.reader;        // Parent reader
 		this.player = queue.reader.player; // Parent player
 
-		this.node = null;
-		this.startsAt = null;
-		this.startsFrom = 0;
-		this.endTimer = null;
+		this.node        = null;
+		this.startsAt    = null;
+		this.startsFrom  = 0;
+		this.endTimer    = null;
 		this.endCallback = null;
 	};
 
@@ -68,59 +95,59 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	 * time at which the node should begin playing according to
 	 * the current time and current content of the parent queue. 
 	 *
-	 * @param int index The index of the current element in the queue.
+	 * @param {int} index The index of the current element in the queue.
 	 */
 	Source.prototype.setup = function (index) {
-		console.log('Setup source', this);
+		// console.log('Setup source', this);
 
 		this.node = this.reader.context.createBufferSource();
 		this.node.buffer = this.buffer;
 		this.node.connect(this.reader.gain);
 
 		if (index > 0) {
-			this.startsAt = this.reader.context.currentTime + this.queue.get(index - 1).remaining();
+			var previous = this.queue.get(index - 1);
+			
+			this.startsAt = previous.startsAt + previous.buffer.duration - previous.startsFrom;
 		} else {
 			this.startsAt = this.reader.context.currentTime;
 		}
 
-		console.log('New startsAt:', this.startsAt);
+		// console.log('New startsAt:', this.startsAt);
 	};
 
 	/**
 	 * Schedules the beginning and the end of the play.
 	 */
 	Source.prototype.schedule = function () {
-		console.log('Schedule source', this);
+		// console.log('Schedule source', this);
 
 		var that = this;
-
-		that.node.start(that.startsAt, that.startsFrom);
 
 		var endsAt = that.startsAt + that.buffer.duration;
 		var endsIn = endsAt - that.reader.context.currentTime;
 
-		console.log('endsIn:', endsIn);
+		that.node.start(that.startsAt, that.startsFrom);
 
 		if (that.endTimer) {
 			clearTimeout(that.endTimer);
 		}
 
 		that.endTimer = setTimeout(function () {
-			console.log('Source ended 100ms ago', that);
+			// console.log('Source ended 200ms ago', that);
 
 			if (that.endCallback) {
 				that.endCallback();
 			}
 
 			that.queue.shift();
-		}, endsIn * 1000 + 100);
+		}, endsIn * 1000 + 200);
 	};
 
 	/**
 	 * Cancels any previously scheduled event.
 	 */
 	Source.prototype.cancel = function () {
-		console.log('Cancel source', this);
+		// console.log('Cancel source', this);
 
 		this.node.stop();
 		clearTimeout(this.endTimer);
@@ -132,8 +159,9 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	 */
 	Source.prototype.destroy = function () {
 		this.cancel();
+
 		delete this.buffer;
-		delete this.queue ;
+		delete this.queue;
 		delete this.reader;
 		delete this.player;
 
@@ -151,13 +179,6 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 		return this.reader.context.currentTime - this.startsAt;
 	};
 
-	/**
-	 * Computes the remaining playing time.
-	 */
-	Source.prototype.remaining = function () {
-		return this.buffer.duration - this.elapsed();
-	};
-
 	// -------------------------------------------------------------------- //
 
 	/**
@@ -169,6 +190,7 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 
 		this.state = STATE_PAUSED;
 		this.sources = [];
+		this.latestElapsed = 0;
 	};
 
 	/**
@@ -211,13 +233,17 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	/**
 	 * Adds a buffer to the queue.
 	 */
-	SourceQueue.prototype.push = function (buffer) {
-		console.log('Pushing to the queue', buffer);
+	SourceQueue.prototype.push = function (buffer, beforeSetup) {
+		// console.log('Pushing to the queue', buffer);
 
 		var source = new Source(buffer, this);
 		var sourceIndex = this.sources.length;
 
 		this.sources.push(source);
+
+		if (typeof beforeSetup !== 'undefined') {
+			beforeSetup(source);
+		}
 
 		if (this.state == STATE_PLAYING) {
 			source.setup(sourceIndex);
@@ -244,6 +270,12 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 			return;
 		}
 
+		this.latestElapsed = null;
+		this.state = STATE_PLAYING;
+		// TODO: Move elsewhere
+		this.reader.playingInterval = setInterval(this.reader.player.whilePlaying, 200);
+		this.reader.player.onPlay();
+
 		for (var i = 0; i < this.sources.length; i++) {
 			var source = this.get(i);
 			source.setup(i);
@@ -259,12 +291,29 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 			return;
 		}
 
+		this.latestElapsed = this.first().elapsed();
+		this.state = STATE_PAUSED;
+		// TODO: Move elsewhere
+		window.clearInterval(this.reader.playingInterval);
+		this.reader.player.onPause();
+
 		for (var i = 0; i < this.sources.length; i++) {
 			this.get(i).cancel();
 		}
 
 		if (!this.isEmpty()) {
-			this.first().startsFrom += this.first.elapsed();
+			this.first().startsFrom += this.first().elapsed();
+		}
+	};
+
+	/**
+	 * Returns the time that was actually elapsed.
+	 */
+	SourceQueue.prototype.elapsed = function () {
+		if (this.latestElapsed !== null) {
+			return this.latestElapsed;
+		} else {
+			return this.first().elapsed();
 		}
 	};
 
@@ -280,20 +329,22 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 
 		this.fragments = [];
 		this.queue = new SourceQueue(this);
-		this.previousFragmentsElapsed = 0.0;
 		this.currentFragmentNumber = 0;
 		this.buffering = false;
 		this.duration = null;
 
 		this.manager = this.player.manager;
 		this.context = this.manager.context;
-		this.gain    = this.manager.gain;
+		this.gain = this.manager.gain;
+
+		this.playingInterval = null;
 
 		var that = this;
 
 		that.fetchMetadata(function () {
-			that.loadAndScheduleFragment(0);
-			that.loadAndScheduleFragment(1);
+			that.loadAndScheduleFragment(0, 12, function () {
+				that.loadAndScheduleFragment(1);
+			});
 		});
 	};
 
@@ -303,13 +354,14 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	Reader.prototype.fetchMetadata = function (callback) {
 		var that = this;
 
-		$.getJSON(this.path, function (metadata) {
+		$.getJSON(this.path, { bust: (new Date()).getTime() }, function (metadata) {
 			that.duration = metadata.duration;
 			
 			$.each(metadata.fragments, function (index, fragment) {
 				that.fragments.push({
 					buffer: null,
-					path: fragment.path,
+					mpath: fragment.mpath,
+					vpath: fragment.vpath,
 					key: fragment.key,
 					iv: fragment.iv
 				});
@@ -344,6 +396,10 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	 * Decodes the given ArrayBuffer.
 	 */
 	Reader.prototype.decodeFragment = function (number, encodedRawBuffer) {
+		if (this.fragments[number].decoded) {
+			return encodedRawBuffer;
+		}
+
 		// https://github.com/wader/aes-arraybuffer
 		var key = this.convertHex(this.fragments[number].key).buffer;
 		var iv  = this.convertHex(this.fragments[number].iv).buffer;
@@ -357,8 +413,13 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	 */
 	Reader.prototype.fetchFragment = function (number, callback) {
 		var that = this;
-		var path = that.fragments[number].path;
 		var request = new XMLHttpRequest();
+
+		if (canPlayVorbis()) {
+			var path = that.fragments[number].vpath;
+		} else {
+			var path = that.fragments[number].mpath;
+		}
 
 		request.open('GET', path, true);
 		request.responseType = 'arraybuffer';
@@ -411,34 +472,45 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	 * The fragment must already be loaded into memory.
 	 */
 	Reader.prototype.scheduleFragment = function (number, offset) {
-		console.log('Scheduling fragment ' + number + ' with offset ' + offset);
+		// console.log('Scheduling fragment ' + number + ' with offset ' + offset);
 
 		var that = this;
 
-		var source = that.queue.push(that.fragments[number].buffer);
+		that.queue.push(that.fragments[number].buffer, function (source) {
+			source.endCallback = function () {
+				// console.log('endCallback called for fragment ' + number);
+				that.currentFragmentNumber = number + 1;
 
-		source.endCallback = function () {
-			that.previousFragmentsElapsed += source.buffer.duration;
-			that.currentFragmentNumber += 1;
-			
-			// Make sure we load the n + 2 fragment when this one ends.
-			that.loadAndScheduleFragment(number + 2);
-		};
+				// Maybe we don't have any fragments left to queue?
+				if (that.currentFragmentNumber >= that.fragments.length) {
+					that.queue.pause();
+					that.queue.state = STATE_FINISHED;
+					that.player.onFinish();
+				}
+				
+				// Make sure we load the n + 2 fragment when this one ends.
+				that.loadAndScheduleFragment(number + 2);
+			};
 
-		// Maybe we must start playing the fragment with a given offset?
-		if (offset) {
-			source.startsFrom = offset;
-		}
+			// Maybe we must start playing the fragment with a given offset?
+			if (offset) {
+				// console.log('startsFrom set to', offset);
+				source.startsFrom = offset;
+			}
+		});
 	};
 
 	/**
 	 * Loads and schedules a given fragment.
 	 */
-	Reader.prototype.loadAndScheduleFragment = function (number, offset) {
+	Reader.prototype.loadAndScheduleFragment = function (number, offset, callback) {
+		callback = callback || function () {};
 		var that = this;
 
 		that.loadFragment(number, function () {
 			that.scheduleFragment(number, offset);
+
+			callback();
 		});
 	};
 
@@ -449,8 +521,6 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	Reader.prototype.state = function () {
 		if (this.buffering) {
 			return STATE_BUFFERING;
-		} else if (this.queue.isEmpty()) {
-			return STATE_ENDED;
 		} else {
 			return this.queue.state;
 		}
@@ -460,7 +530,7 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	 * Computes the total elapsed time.
 	 */
 	Reader.prototype.getCurrentTime = function () {
-		return this.previousFragmentsElapsed + this.queue.first().elapsed();
+		return this.currentFragmentNumber * FRAGMENT_DURATION + this.queue.elapsed();
 	};
 
 	/**
@@ -471,13 +541,22 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 
 		var number = that.getFragmentNumber(time);
 		var offset = time % FRAGMENT_DURATION;
+		that.currentFragmentNumber = number;
 
+		that.queue.pause();
 		that.buffering = true;
+		that.player.onBufferingStart();	
+		
 		that.queue.empty();
 
 		that.loadFragment(number, function () {
+			that.player.onBufferingStop();
 			that.buffering = false;
+			that.queue.play();
+
 			that.scheduleFragment(number, offset);
+
+			that.loadAndScheduleFragment(number + 1);
 		});
 	};
 
@@ -507,22 +586,12 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	};
 
 	Player.prototype = {
-		// Player options
-		autoplay: false,
-		loop: false,
-
-		// Player current state
-		properties: {
-			muted: false,
-			volume: 1.0,
-		},
-
 		get paused() {
 			return this.reader.state() == STATE_PAUSED;
 		},
 
-		get ended() {
-			return this.reader.state() == STATE_ENDED;
+		get finished() {
+			return this.reader.state() == STATE_FINISHED;
 		},
 
 		get currentTime() {
@@ -544,8 +613,8 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 		onPause: function() {
 			console.log('onPause');
 		},
-		onEnded: function() {
-			console.log('onEnded');
+		onFinish: function() {
+			console.log('onFinish');
 		},
 		onBufferingStart: function() {
 			console.log('onBufferingStart');
@@ -563,7 +632,6 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	 */
 	Player.prototype.play = function () {
 		this.reader.queue.play();
-		this.onPlay();
 	};
 
 	/**
@@ -571,7 +639,6 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	 */
 	Player.prototype.pause = function () {
 		this.reader.queue.pause();
-		this.onPause();
 	};
 
 	/**
@@ -654,7 +721,7 @@ define(['jquery', 'Crypto'], function ($, Crypto) {
 	 */
 	Manager.prototype.setVolume = function (volume) {
 		this.volume = volume;
-		this.reader.gain.gain.value = this.properties.volume;
+		this.gain.gain.value = volume;
 
 		this.onVolumeChange();
 	};
